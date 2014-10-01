e14a0d3b2072b81e51276dd3bb27beea4d4b6dea
Raul Aviles <raulaviles@Rauls-MacBook-Pro-2.local>
2014-10-01 08:33:18 -0500

    Initial Commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Raul Aviles on 10/1/14.
+//  Copyright (c) 2014 Palm3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
